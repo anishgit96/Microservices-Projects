f281c562911c9609ec7a6751650e41bf7b04701e
Anish Sethiya <anishsethiya22@gmail.com>
2021-05-25 20:09:51 +0530

    Microservices Step4

---
+++ step4/springcloudconfigserver/src/test/java/com/example/microservices/springcloudconfigserver/SpringcloudconfigserverApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+class SpringcloudconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
